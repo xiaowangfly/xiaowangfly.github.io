cf57ab9d0f8a6d47751f689e902a8eca7193fa3c
xiaowangfly <107753601+xiaowangfly@users.noreply.github.com>
2023-11-06 15:09:33 +0800

    Add files via upload

--- js/click.js
+++ js/click.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 
 jQuery(document).ready(function($) {
 	$("body").click(function(e) {
-		var a = new Array("小王Fly","小王Bot","LiChen","小源151","Explorer_Chils","Leo","Hope解说","BadJui","0殺神在心0","Minecraft","我的世界","BSOD-SYSTEM","爱玩机的SYSTEM-QCX","乐乐QWQLL404","正在喝可乐的九月","DataCleaner");
+		var a = new Array("小王Fly","小王Bot","LiChen","小源151","Explorer_Chils","Leo","Hope解说","BadJui","0殺神在心0","♥Minecraft♥","♥我的世界♥","♥原神♥","BSOD-SYSTEM","爱玩机的SYSTEM-QCX","乐乐QWQLL404","正在喝可乐的九月","DataCleaner","zilinkids");
 		var $i = $("<span/>").text(a[a_idx]);
 		a_idx = (a_idx + 1) % a.length;
 		var x = e.pageX, y = e.pageY;
